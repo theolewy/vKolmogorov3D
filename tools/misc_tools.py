ca1a335fde15dbac84b595f488104a664fd59cc1
Theo Lewy <theolewy@gmail.com>
2025-01-22 21:59:00 +0000

    r

--- tools/misc_tools.py
+++ tools/misc_tools.py
@@ -54,10 +54,10 @@ def get_ic_file(material_params, system_params, solver_params, restart=False, su
 
     if ic_file is None and ic_dict_if_reinit is not None:
         if 'suffix' in ic_dict_if_reinit.keys(): 
-            suffix = kwargs['suffix']
+            suffix = ic_dict_if_reinit['suffix']
             del ic_dict_if_reinit['suffix']
         if 'subdir' in ic_dict_if_reinit.keys(): 
-            subdir = kwargs['subdir']
+            subdir = ic_dict_if_reinit['subdir']
             del ic_dict_if_reinit['subdir']
         ic_file, noise_coeff = get_ic_file(material_params, system_params, solver_params, restart=False, closest_made_to_params=False,
                     suffix=suffix, subdir=subdir, ic_dict_if_reinit=None, **ic_dict_if_reinit)
