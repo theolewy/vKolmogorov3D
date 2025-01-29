0846dfe4477a4885eb345b5d418dcf95b641fa77
Theo Lewy <theolewy@gmail.com>
2025-01-29 23:06:48 +0000

    r

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -89,7 +89,7 @@ elif setting_mode == 6:
     system_params['Lx'] = 4 * np.pi
     solver_params['Nx'] = 128
     solver_params['dt'] = 2e-3
-    ic_dict_if_reinit = {'ndim': 2, 'Nx': 64, 'suffix':'recent-', 'subdir': 'arrowhead_2D', 'noise_coeff':1e-3}
+    ic_dict_if_reinit = {'ndim': 2, 'Nx': 128, 'suffix':'recent-', 'subdir': 'arrowhead_2D', 'noise_coeff':1e-3}
     symmetry_mode = 'yz'
     suffix_end = f'symmetry-yz'
 
