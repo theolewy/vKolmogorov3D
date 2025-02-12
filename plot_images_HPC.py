e212d5a50b9548827d6e8e4b287e89151112f304
Theo Lewy <theolewy@gmail.com>
2025-02-12 17:07:57 +0000

    r

--- plot_images_HPC.py
+++ plot_images_HPC.py
@@ -1,4 +1,5 @@
 from tools.plotter import *
 import matplotlib.pyplot as plt
 
-check_localised()
+check_localised(W=20, eps=1e-3, beta=0.9, L=np.inf, Re=0.5, Lx=3*np.pi, Lz=4*np.pi,  Nx=64, Ny=64, Nz=64, suffix='recent-localised', subdir='arrowhead_3D')
+check_localised(W=20, eps=2e-4, beta=0.9, L=np.inf, Re=0.5, Lx=3*np.pi, Lz=4*np.pi,  Nx=128, Ny=128, Nz=128, suffix='recent-localised', subdir='arrowhead_3D')
