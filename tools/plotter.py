955647a1710a129f66fdb6a8108662b458a2ba16
Theo Lewy <theolewy@gmail.com>
2025-02-18 22:50:44 +0000

    fix: adjust field_array calculation for correct dimensionality in plotter.py

--- tools/plotter.py
+++ tools/plotter.py
@@ -229,7 +229,7 @@ def check_localised(W, eps, beta, L, Re, Lx, Lz,  Nx, Ny, Nz, suffix='', subdir=
 
     for field_name in fields:
         base_field = base_flow[field_name]
-        field_array = data_fields[field_name][-1,:,:,:] - base_field
+        field_array = data_fields[field_name][-1,:,:,:] - base_field[:, None, :]
         field_int = np.linalg.norm(field_array, axis=(0,2))
         field_int /= np.max(field_int)
 
