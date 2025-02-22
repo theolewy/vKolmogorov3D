27126062f0e0c4fe22207ad708c0ee9a25662f3a
Theo Lewy <theolewy@gmail.com>
2025-02-22 10:04:59 +0000

    fix: add translate parameter handling for mode 13 in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -292,6 +292,7 @@ elif setting_mode == 12:
     suffix_end = 'localised'
     symmetry_mode = False
     translate = True
+
 elif setting_mode == 13:
 
     material_params['W'] = 20
@@ -306,6 +307,7 @@ elif setting_mode == 13:
 
     ic_dict_if_reinit = {'Nx': 64, 'Lx': 3*np.pi, 'subdir':'arrowhead_3D', 'suffix': 'recent-localised'}
     suffix_end = f'localised-xy'
+    translate = False
 
 log_all_params(material_params, system_params, solver_params)
 
