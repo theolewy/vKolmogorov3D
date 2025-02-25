fix: improve index calculation for mode 'x' in TimeStepper3D using pressure gradient

tools/solvers/kolmogorov.py
@@ -599,8 +599,14 @@ class TimeStepper3D(CartesianTimeStepper):
             for field_name in self.variables:
                 flow_translated[field_name] = np.roll(flow[field_name], shift=self.Nz//2-idx, axis=1)
         elif mode == 'x':
-            c22_x = np.max(flow['c22'], axis=(1,2))
-            idx = np.argmin(c22_x)
+            # if AH is at y=-pi and pi rather than 0.
+            mean_c22_y = np.mean(self.c22['g'], axis=(0,1))
+            if mean_c22_y[0] > mean_c22_y[self.Ny//2]:
+                p_centre_line = flow['p'][: , self.Nz//2 ,0]
+            else:
+                p_centre_line = flow['p'][: , self.Nz//2 ,self.Ny//2]
+            p_diff = np.diff(p_centre_line, n=1)
+            idx = np.argmax(np.abs(p_diff))
             
             for field_name in self.variables:
                 flow_translated[field_name] = np.roll(flow[field_name], shift=self.Nx//2-idx, axis=0)