f8c0775ff54e740e5e750fda0bb3b25151bd255e
Theo Lewy <theolewy@gmail.com>
2025-02-27 16:15:05 +0000

    fix: add setting mode 12 for Saddle truncation in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -142,6 +142,16 @@ elif setting_mode == 11:
 
     ic_dict_if_reinit = {'Lz':8*np.pi, 'Nz':128, 'suffix': 'recent-localised'}
     suffix_end = 'localised-contracted'
+elif setting_mode == 12:
+    # Truncate to Saddle
+    # Reduce Lz from 8pi down. Nz MUST be over 16 per pi in Lz
+
+    solver_params['Nz'] = 64
+    system_params['Lz'] =  3.7*np.pi
+
+    kwargs = {'truncate': True}
+    ic_dict_if_reinit = {'Lz': 3.75*np.pi}
+    suffix_end = 'localised-yz'
 
 log_all_params(material_params, system_params, solver_params)
 
