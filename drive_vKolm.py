9b66d6d97b440bfe3628935644f3b644fff3e564
Theo Lewy <theolewy@gmail.com>
2025-03-02 20:17:49 +0000

    fix: adjust solver and system parameters for periodic AH settings in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -251,11 +251,12 @@ elif setting_mode == 23:
 elif setting_mode == 24:
     # Get Periodic AH from 2D AH. m=1 mode branch
 
-    solver_params['Nz'] = 64
-    system_params['Lz'] =  4*np.pi
+    solver_params['Nz'] = 32
+    system_params['Lz'] =  input_val*np.pi
 
-    ic_dict_if_reinit = {'Lz': 3*np.pi, 'Nz': 64}
+    ic_dict_if_reinit = {'Lz': np.pi, 'Nz': 32}
     suffix_end = 'periodic-yz'
+
 elif setting_mode == 25:
     # Get Periodic AH from 2D AH. m=1 mode branch
 
