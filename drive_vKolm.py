ed9fbb5f93137a5de246996cc932bc3f2ece7df7
Theo Lewy <theolewy@gmail.com>
2025-03-03 16:48:42 +0000

    fix: update solver and initial conditions for periodic AH settings in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -182,10 +182,10 @@ elif setting_mode == 15:
 elif setting_mode == 16:
     # Get Periodic AH from 2D AH. m=1 mode branch
 
-    solver_params['Nz'] = 64
-    system_params['Lz'] =  3.5*np.pi
+    solver_params['Nz'] = 96
+    system_params['Lz'] =  input_val*np.pi
 
-    ic_dict_if_reinit = {'Lz': 3*np.pi, 'Nz': 64}
+    ic_dict_if_reinit = {'Lz': 5*np.pi, 'Nz': 96}
     suffix_end = 'periodic-yz'
 elif setting_mode == 17:
     # Get Periodic AH from 2D AH. m=1 mode branch
