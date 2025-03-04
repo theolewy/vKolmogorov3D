bd8c710fe54341147f0e0b10528a88931059c84c
Theo Lewy <theolewy@gmail.com>
2025-03-04 19:25:50 +0000

    fix: update solver time step to 1e-2 and Lz parameter to 6*np.pi for improved simulation accuracy in drive_vKolm_windowed.py

--- drive_vKolm_windowed.py
+++ drive_vKolm_windowed.py
@@ -154,9 +154,9 @@ elif setting_mode == 14:
     a, b = 2*np.pi/3, np.pi/2
 
     material_params['W'] = 20
-    solver_params['dt'] = 8e-3
+    solver_params['dt'] = 1e-2
 
-    system_params['Lz'] = 4*np.pi
+    system_params['Lz'] = 6*np.pi
     system_params['Lx'] = 24*np.pi
 
     solver_params['Nz'] = 32
