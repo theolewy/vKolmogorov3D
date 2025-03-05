c70c12af86547e1e53561306eed3ffbb4635bb1b
Theo Lewy <theolewy@gmail.com>
2025-03-05 15:36:46 +0000

    fix: reduce solver_params['dt'] to 1e-2 for improved simulation stability in setting_mode 14 and 15

--- drive_vKolm_windowed.py
+++ drive_vKolm_windowed.py
@@ -155,7 +155,7 @@ elif setting_mode == 14:
     a, b = 3*np.pi/3, np.pi*3/4
 
     material_params['W'] = 20
-    solver_params['dt'] = 2e-2
+    solver_params['dt'] = 1e-2
 
     system_params['Lz'] = 6*np.pi
     system_params['Lx'] = 24*np.pi
@@ -176,7 +176,7 @@ elif setting_mode == 15:
     a, b = 3*np.pi/3, np.pi*3/4
 
     material_params['W'] = 20
-    solver_params['dt'] = 2e-2
+    solver_params['dt'] = 1e-2
 
     system_params['Lz'] = 6*np.pi
     system_params['Lx'] = 30*np.pi
