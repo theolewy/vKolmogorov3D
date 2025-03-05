a2643c0726c5f4db75f80608d441942bdc62b0fa
Theo Lewy <theolewy@gmail.com>
2025-03-05 21:54:24 +0000

    fix: add dt parameter for improved simulation time step in setting_mode 15

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -190,6 +190,7 @@ elif setting_mode == 15:
 
     solver_params['Nz'] = 64
     system_params['Lz'] =  3*np.pi
+    solver_params['dt'] = 5e-3
 
     ic_dict_if_reinit = {'Lz': 3*np.pi, 'Nz': 64}
     suffix_end = 'periodic-yz'
