939fc1183c10ec11c809ee3af34194338496099b
Theo Lewy <theolewy@gmail.com>
2025-03-05 10:14:16 +0000

    fix: add functionality to merge process files from edge_track subdirectories in merge_runs.py

--- merge_runs.py
+++ merge_runs.py
@@ -26,6 +26,23 @@ for subdirectory in subdirectories:
             # split_path = os.path.join(base_path, split_dir)
             # post.merge_process_files(split_path, cleanup=True)
 
+edge_path = os.path.join(data_root, 'edge_track')
+subdirectories = os.listdir(edge_path)
+
+for subdirectory in subdirectories:
+    field1_path = os.path.join(simulations_path, subdirectory, 'field1')
+    field2_path = os.path.join(simulations_path, subdirectory, 'field2')
+
+    for base_dir in os.listdir(field1_path):
+        base_path = os.path.join(subdirectory_path, base_dir)
+        post.merge_process_files(base_path, cleanup=True)
+    for base_dir in os.listdir(field2_path):
+        base_path = os.path.join(subdirectory_path, base_dir)
+        post.merge_process_files(base_path, cleanup=True)
+        # for split_dir in os.listdir(base_path):
+            # split_path = os.path.join(base_path, split_dir)
+            # post.merge_process_files(split_path, cleanup=True)
+
 
 
 # simulations_files = os.listdir(simulations_path)
