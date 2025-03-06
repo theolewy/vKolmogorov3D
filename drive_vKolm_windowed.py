f84a6d644b210b681d5afea0da4e371e556e36f7
Theo Lewy <theolewy@gmail.com>
2025-03-06 22:44:38 +0000

    fix: update 'Lz' and 'Nz' parameters for improved simulation in setting_mode 10 and 11

--- drive_vKolm_windowed.py
+++ drive_vKolm_windowed.py
@@ -84,16 +84,17 @@ elif setting_mode == 10:
     material_params['W'] = 20
     solver_params['dt'] = 1e-2
 
-    system_params['Lz'] = 4*np.pi
+    system_params['Lz'] = 6*np.pi
     system_params['Lx'] = 6*np.pi
 
-    solver_params['Nz'] = 32
+    solver_params['Nz'] = 48
     solver_params['Ny'] = 32
     solver_params['Nx'] = 64
 
     ic_dict_if_reinit = {'Nx': 64, 'Ny': 64, 'Nz': 64, 'Lx': 3*np.pi, 'suffix': f'recent-localised-yz', 'subdir': 'arrowhead_3D'}
     suffix_end = f'localised-yz'
     window_mode = 'x'
+    tile = True
 
 elif setting_mode == 11:
     # Localising in a spanwise localised soln in x direction...
@@ -102,16 +103,17 @@ elif setting_mode == 11:
     material_params['W'] = 20
     solver_params['dt'] = 1e-2
 
-    system_params['Lz'] = 4*np.pi
+    system_params['Lz'] = 6*np.pi
     system_params['Lx'] = 4*np.pi
 
-    solver_params['Nz'] = 32
+    solver_params['Nz'] = 48
     solver_params['Ny'] = 32
     solver_params['Nx'] = 64
 
     ic_dict_if_reinit = {'Nx': 64, 'Ny': 64, 'Nz': 64, 'Lx': 3*np.pi, 'suffix': f'recent-localised-yz', 'subdir': 'arrowhead_3D'}
     suffix_end = f'localised-yz'
     window_mode = False
+    tile = False
 
 elif setting_mode == 12:
     # Localising in a spanwise localised soln in x direction...
