63c36aa38c282148680f8c268ce29cb674929ed7
Theo Lewy <theolewy@gmail.com>
2025-03-11 15:29:26 +0000

    fix: update Nx and Lx parameters in drive_vKolm.py for enhanced simulation accuracy

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -281,10 +281,10 @@ elif setting_mode == 27:
     # Get Periodic AH from 2D AH. m=1 mode branch
     
     system_params['ndim'] = 2
-    solver_params['Nx'] = 420
+    solver_params['Nx'] = 512
     solver_params['Ny'] = 64
 
-    system_params['Lx'] = 25*np.pi
+    system_params['Lx'] = 35*np.pi
     solver_params['dt'] = 5e-3
 
     ic_dict_if_reinit = {'Lx': 30*np.pi,'Nx': 512, 'subdir': 'arrowhead_2D', 'suffix': 'recent-'}
