0a8c740cae6055228d8781e3527399b000da56b3
Theo Lewy <theolewy@gmail.com>
2025-03-11 15:31:29 +0000

    fix: update initial conditions in drive_vKolm.py for improved simulation parameters

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -288,7 +288,6 @@ elif setting_mode == 27:
     solver_params['dt'] = 5e-3
 
     ic_dict_if_reinit = {'Lx': 30*np.pi,'Nx': 512, 'subdir': 'arrowhead_2D', 'suffix': 'recent-'}
-    # ic_dict_if_reinit = {'Lx': 3*np.pi,'Nx': 128, 'Ny': 256, 'subdir': 'arrowhead_2D', 'suffix': 'recent-'}
     suffix_end = ''
     plot_subdirectory = 'streamwise_localisation'
     symmetry_mode = 'y'
