33946caf8d98150437c79d68d6608adaac90bd75
Theo Lewy <theolewy@gmail.com>
2025-03-20 13:02:01 +0000

    fix: update Nz parameter and initial conditions for setting mode in drive_vKolm_windowed.py

--- drive_vKolm_windowed.py
+++ drive_vKolm_windowed.py
@@ -133,16 +133,16 @@ elif setting_mode == 11:
 
     solver_params['Nx'] = 480
     solver_params['Ny'] = 64
-    solver_params['Nz'] = 48
+    solver_params['Nz'] = 96
 
     system_params['Lx'] = 32*np.pi
     system_params['Lz'] = 5*np.pi
     solver_params['dt'] = 5e-3
 
-    ic_dict_if_reinit = {'Nx': 480, 'Ny':64, 'Nz': 96}
+    ic_dict_if_reinit = {'Nx': 480, 'Ny':48, 'Nz': 16, 'Lz': np.pi, 'suffix': f'recent-', 'subdir': 'localisation'}
     suffix_end = f'windowed'
-    window_mode = False
-    tile = False
+    window_mode = 'z'
+    tile = True
 
 log_all_params(material_params, system_params, solver_params)
 
