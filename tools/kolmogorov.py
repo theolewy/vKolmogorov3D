35d5d1405f09e858a8605e8a00711e0519e8f4ef
Theo Lewy <theolewy@gmail.com>
2025-03-27 14:13:20 +0000

    fix: remove redundant logging and reset history cache in TimeStepper3D class

--- tools/kolmogorov.py
+++ tools/kolmogorov.py
@@ -658,11 +658,9 @@ class TimeStepper3D(CartesianTimeStepper):
         for field_name, base_field_name in zip(self.variables, base_flow_variable_names):
             field = getattr(self, field_name)
 
-            logger.info(field_name)
-            logger.info(self.Nz//2)
-
             field['g'] = (field['g'] + np.roll(field['g'], shift=self.Nz//2, axis=1)) / 2
 
+        self._reset_history_cache() # whenever we change the state, must forget history of implicit timestepper
 
 
     def _window_field(self, field, base, a, b, mode='z'):
