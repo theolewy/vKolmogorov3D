d8a4503eacda0614708b79d8ce7eaadc8beed47c
Theo Lewy <theolewy@gmail.com>
2025-03-27 10:18:43 +0000

    fix: update initial condition parameter W for setting_mode 19 and 20 in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -263,7 +263,7 @@ elif setting_mode == 19:
     
     solver_params['dt'] = 5e-3
 
-    ic_dict_if_reinit = {'W': 28}
+    ic_dict_if_reinit = {'W': 16}
     suffix_end = 'localised'
     plot_subdirectory = 'arrowhead_3D_W'
 
@@ -277,7 +277,7 @@ elif setting_mode == 20:
     
     solver_params['dt'] = 5e-3
 
-    ic_dict_if_reinit = {'W': 16}
+    ic_dict_if_reinit = {'W': 16.5}
     suffix_end = 'localised'
     plot_subdirectory = 'arrowhead_3D_W'
 
