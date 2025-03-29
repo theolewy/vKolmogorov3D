709b655159d8420ba1d20a6691e7693b40b30555
Theo Lewy <theolewy@gmail.com>
2025-03-29 09:42:36 +0000

    fix: update initial condition for W in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -266,7 +266,7 @@ elif setting_mode == 19:
     
     solver_params['dt'] = 5e-3
 
-    ic_dict_if_reinit = {'W': 24}
+    ic_dict_if_reinit = {'W': 15.3}
     suffix_end = 'localised'
     plot_subdirectory = 'arrowhead_3D_W'
 
