1963f32b25dd5bf960547f796adac68bc55a17e8
Theo Lewy <theolewy@gmail.com>
2025-03-30 22:28:47 +0100

    fix: update initial condition for Lz in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -171,7 +171,7 @@ elif setting_mode == 11:
     
     solver_params['dt'] = 5e-3
 
-    ic_dict_if_reinit = {'Lz': 3.2*np.pi, 'Nz': 64}
+    ic_dict_if_reinit = {'Lz': 2.8*np.pi}
     suffix_end = 'localised'
     plot_subdirectory = 'arrowhead_3D_W'
 
