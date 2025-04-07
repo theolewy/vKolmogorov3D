b6a74b504daf06983a1800b02c9937008ab640f6
Theo Lewy <theolewy@gmail.com>
2025-04-07 11:17:12 +0100

    fix: update material parameters and plot subdirectory for setting mode 19 in drive_vKolm.py

--- drive_vKolm.py
+++ drive_vKolm.py
@@ -275,17 +275,16 @@ elif setting_mode == 18:
 
 elif setting_mode == 19:
     # Get Periodic AH from 2D AH. m=1 mode branch
+    material_params['W'] = 13
 
-    material_params['W'] = input_val
-
-    system_params['Lz'] = 3*np.pi
+    system_params['Lz'] = input_val*np.pi
     solver_params['Nz'] = 48
     
     solver_params['dt'] = 5e-3
 
-    ic_dict_if_reinit = {'W': 15}
+    ic_dict_if_reinit = {'W':14}
     suffix_end = 'localised'
-    plot_subdirectory = 'arrowhead_3D_W'
+    plot_subdirectory = 'arrowhead_3D_W=13'
 
 elif setting_mode == 20:
     # Get Periodic AH from 2D AH. m=1 mode branch
