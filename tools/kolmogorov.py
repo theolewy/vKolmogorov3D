16515d953ab5f5a62b1d4d1964eeb1016fe4b848
Theo Lewy <theolewy@gmail.com>
2025-04-08 09:08:40 +0100

    fix: update coordinate assignment in prime_coords_to_unprimed method of TimeStepper3D

--- tools/kolmogorov.py
+++ tools/kolmogorov.py
@@ -665,7 +665,7 @@ class TimeStepper3D(CartesianTimeStepper):
     def prime_coords_to_unprimed(self, C):
 
         x_prime, y_prime, z_prime = self.x.squeeze(), self.y.squeeze(), self.z.squeeze()
-        x, y, z = x_prime, y_prime, z_prime + C * x_prime
+        x, y, z = self.x, self.y, self.z + C * self.x
 
         print(x.shape, x_prime.shape)
 
