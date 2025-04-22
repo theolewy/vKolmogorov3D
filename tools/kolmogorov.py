fix: modify array calculation in TimeStepper3D to adjust perturbation effect

tools/kolmogorov.py
@@ -392,11 +392,10 @@ class TimeStepper3D(CartesianTimeStepper):
 
             mag = kwargs['asymmetric_perturb']
 
-
             field = self.c11
             array = self.get_full_array(field['g'])
             z = self.get_full_array(self.z)
-            array += mag * np.max(array) * np.sin(2 * np.pi * z / self.Lz)
+            array *= (1 - mag * np.max(array) * np.sin(2 * np.pi * z / self.Lz))
             field['g'] = array[local_slice]
 
             self._reset_history_cache()