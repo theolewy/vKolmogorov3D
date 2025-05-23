751e9eff28d776b2ff257ea686d49197469de565
Theo Lewy <theolewy@gmail.com>
2025-05-23 19:41:18 +0100

    fix: clean up commented imports in temp.py for better readability

--- tools/temp.py
+++ tools/temp.py
@@ -2,11 +2,10 @@ import copy
 import os
 import pickle
 
-import numpy as np
+# import numpy as np
 import logging
 import time
+# from cfd_tools.cartesian_systems.misc_tools import *
 from dedalus.tools import post
 import socket
-
-# from cfd_tools.cartesian_systems.misc_tools import *
 
