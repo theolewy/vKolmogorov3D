7cc30f25224606b3fa0d425508b6474917f2e874
Theo Lewy <theolewy@gmail.com>
2025-05-23 19:42:27 +0100

    fix: remove unused imports in temp.py for improved clarity

--- tools/temp.py
+++ tools/temp.py
@@ -1,11 +1,3 @@
-import copy
-import os
-import pickle
+from dedalus.tools import post
 
-# import numpy as np
-import logging
-import time
-# from cfd_tools.cartesian_systems.misc_tools import *
-# from dedalus.tools import post
-import socket
 
