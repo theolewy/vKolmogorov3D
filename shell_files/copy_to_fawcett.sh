3dcb344d8773f40c4c1dc4c2bce1a183a1bc72cb
Theo Lewy <theolewy@gmail.com>
2025-09-17 15:16:58 +0200

    fix: correct command typo in copy_to_fawcett.sh

--- shell_files/copy_to_fawcett.sh
+++ shell_files/copy_to_fawcett.sh
@@ -5,4 +5,4 @@ eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3
 eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2/"
 
 eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2"
-eval "p_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2"
+eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2"
