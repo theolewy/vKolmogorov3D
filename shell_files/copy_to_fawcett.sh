1f704e8d437a77471ae12210c4f634e1ce7fcc6e
Theo Lewy <theolewy@gmail.com>
2025-09-17 15:11:18 +0200

    feat: add script to manage simulation files in arrowhead_3D directory

---
+++ shell_files/copy_to_fawcett.sh
@@ -0,0 +1,8 @@
+cd_nfs
+cd vKolmogorov3D/simulations/arrowhead_3D
+
+rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2/
+rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2/
+
+cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2
+cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2
