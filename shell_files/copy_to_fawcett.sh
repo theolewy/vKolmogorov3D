abaa50c0649b7c5a3af9f11619a5fe52dbc05490
Theo Lewy <theolewy@gmail.com>
2025-09-17 15:14:52 +0200

    fix: wrap commands in eval for proper execution in copy_to_fawcett.sh

--- shell_files/copy_to_fawcett.sh
+++ shell_files/copy_to_fawcett.sh
@@ -1,8 +1,8 @@
-cd_nfs
-cd vKolmogorov3D/simulations/arrowhead_3D
+eval "cd_nfs"
+eval "cd vKolmogorov3D/simulations/arrowhead_3D"
 
-rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2/
-rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2/
+eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2/"
+eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2/"
 
-cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2
-cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2
+eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2"
+eval "p_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2"
