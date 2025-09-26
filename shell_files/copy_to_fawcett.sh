6968953d05a46de15f6540870c4e3d70903ab752
Theo Lewy <theolewy@gmail.com>
2025-09-26 09:32:17 +0100

    fix: update removal commands for drift-perturbation simulations to correct versioning

--- shell_files/copy_to_fawcett.sh
+++ shell_files/copy_to_fawcett.sh
@@ -13,8 +13,8 @@ eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3
 eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,02-method-2"
 eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,02-method-2"
 
-eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,031-method-2/"
-eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,031-method-2/"
+eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,03-method-2/"
+eval "rm -rf sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,03-method-2/"
 
 eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_long-test-drift-pert-0,03-method-2"
 eval "cp_from_swirles vKolmogorov3D/simulations/arrowhead_3D/ sim_W_20_Re_0,5_beta_0,9_eps_0,001_L_inf_Lx_9,4248_Lz_12,566_ndim_3_N_64-64-64_recent-test-drift-pert-0,03-method-2"
